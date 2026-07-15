d10bb03bfdc04bc8b04c7fbbd6d5374a04d93cdf
yuu-abc <yuuyuu1981@gmail.com>
2026-07-16 06:44:59 +0900

    Fix mobile install launch URL

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'today3-v2';
+const APP_SHELL = [
+  './index.html',
+  './today3.webmanifest',
+  './today3-180.png',
+  './today3-192.png',
+  './today3-512.png',
+  './today3-maskable-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html')))
+  );
+});
